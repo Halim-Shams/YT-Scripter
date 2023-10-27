bd653fb0583b9e4f786326d17eb9bc50dc33b9e0
exit <halimshams8@gmail.com>
2023-10-27 15:23:20 +0430

    url input added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Input = () => {
-	return <div>Input</div>;
+	return (
+		<form className='w-[38rem] mx-auto'>
+			<input
+				type='url'
+				placeholder='Enter the URL...'
+				className='rounded-lg focus:border-yellow-400 hover:shadow-md focus:shadow-xl transition-all text-gray-600 outline-none border py-2 px-4 text-xl font-mono w-full'
+			/>
+		</form>
+	);
 };
 
 export default Input;
--- src/pages/UrlPage.jsx
+++ src/pages/UrlPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Input from '../components/Input';
 
 const UrlPage = () => {
-	return <div>UrlPage</div>;
+	return (
+		<div className='mt-44'>
+			<Input />
+		</div>
+	);
 };
 
 export default UrlPage;
