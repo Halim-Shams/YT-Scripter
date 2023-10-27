b4f41244a318a51749cf679da4ca8eedb9231599
exit <halimshams8@gmail.com>
2023-10-27 15:30:12 +0430

    script page added

---
+++ src/components/ScriptInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ScriptInput = () => {
+	return (
+		<div className='w-[38rem] mx-auto'>
+			<p className='text-5xl font-mono text-gray-400 text-center'>
+				Comming Soon!
+			</p>
+		</div>
+	);
+};
+
+export default ScriptInput;
--- src/pages/ScriptPage.jsx
+++ src/pages/ScriptPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ScriptInput from '../components/ScriptInput';
 
 const ScriptPage = () => {
-	return <div>ScriptPage</div>;
+	return (
+		<div className='mt-20'>
+			<ScriptInput />
+		</div>
+	);
 };
 
 export default ScriptPage;
